6b5b0f6118006ee7e421efc223ee578744f97208
Wei Yang <yangw@slac.stanford.edu>
2017-07-21 23:46:19 -0700

    stat of cache by cmsd via oss.statlib

---
+++ XrdOssStatInfoDCP.cc
@@ -0,0 +1,49 @@
+/*
+ * Author: Wei Yang (SLAC National Accelerator Laboratory / Stanford University, 2017)
+ */
+
+using namespace std;
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string>
+
+#include "pfn2cache.hh"
+#include "/usr/include/xrootd/XrdVersion.hh"
+#include "XrdOss/XrdOss.hh"
+#include "XrdOss/XrdOssStatInfo.hh"
+#include "XrdOuc/XrdOucEnv.hh"
+#include "XrdSys/XrdSysError.hh"
+
+extern "C" {
+
+int XrdOssStatInfo(const char *path, struct stat *buff,
+                   int         opts, XrdOucEnv   *envP)
+{
+     std::string cachePath, prefix, tmp;
+     std::size_t i;
+
+     tmp = path;
+     i = tmp.rfind("/atlas/rucio");
+     if (i == string::npos) 
+         cachePath = path;
+     else
+     {
+         prefix = tmp.substr(0, i);
+         cachePath = prefix + pfn2cache("", tmp.substr(i+1, tmp.length() -i).c_str());
+     }
+     return (stat(cachePath.c_str(), buff)? -1 : 0);
+}
+
+XrdOssStatInfo_t XrdOssStatInfoInit(XrdOss        *native_oss,
+                                    XrdSysLogger  *Logger,
+                                    const char    *config_fn,
+                                    const char    *parms)
+{
+    return (XrdOssStatInfo_t)XrdOssStatInfo;
+}
+};
+
+XrdVERSIONINFO(XrdOssStatInfoInit,Stat-DCP-for-RUCIO);
